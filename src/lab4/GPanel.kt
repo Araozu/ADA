57e496915aa06c56ee23dec7df71738724339793
Araozu <tbyuyfcjjll@gmail.com>
2019-11-24 17:42:54 -0500

    Cambiado comportamiento por el cual se muestra el grafico: - En cada repaint() se re-dibuja el state actual - La funcion swap() ya no se pasa al algoritmo, sino doRepaint() que re-dibuja el state.

--- src/lab4/GPanel.kt
+++ src/lab4/GPanel.kt
@@ -4,63 +4,67 @@ import java.awt.Color
 import java.awt.Graphics
 import java.awt.Graphics2D
 import java.awt.geom.Rectangle2D
+import java.util.*
 import javax.swing.JPanel
-import javax.swing.Timer
 
 class GPanel(private val elems: Array<Int>, val stepTime: Int) : JPanel() {
 
-    private val squares = Array<Rectangle2D?>(elems.size) { null }
-    private var inicializado = false
-    private var heightRatio = this.height / 60.0
-    private var widthRatio  = this.width  / 50.0
+    private var heightRatio = this.height / elems.size.toDouble()
+    private var widthRatio = this.width / elems.size.toDouble()
+
+    private fun doRepaint() {
+        this.repaint()
+    }
 
     private fun swap(pos1: Int, pos2: Int) {
-        val rectangle1 = squares[pos1] ?: return
-        val rectangle2 = squares[pos2] ?: return
+        val temp = elems[pos1]
+        elems[pos1] = elems[pos2]
+        elems[pos2] = temp
+    }
 
-        val minH = rectangle1.minX
-        rectangle1.setRect(rectangle2.minX, rectangle1.minY, widthRatio, rectangle1.height)
-        rectangle2.setRect(minH, rectangle2.minY, widthRatio, rectangle2.height)
-
-        //change position of shapes in array
-        val temp = squares[pos1]
-        squares[pos1] = squares[pos2]
-        squares[pos2] = temp
-        this.repaint()
-
+    fun finaliseUpdate(millisecondDelay: Long) {
+        repaint()
+        try {
+            Thread.sleep(millisecondDelay)
+        } catch (ex: InterruptedException) {
+            Thread.currentThread().interrupt()
+        }
     }
 
     fun sortAsync(algo: SortAlgorithm) {
 
-        var t: Timer? = null
-        t = Timer(stepTime) {
-            if (!algo.isSorted()) {
-                algo.step(::swap)
-            } else {
-                println("Stopping timer from ${Thread.currentThread().name}")
-                t?.stop()
-            }
+        while (!algo.isSorted()) {
+            algo.step(::doRepaint)
+            finaliseUpdate(stepTime.toLong())
         }
-        t.start()
 
     }
 
-    private fun inicializar(ga: Graphics2D) {
+    fun shuffleArray() {
+        val rn = Random()
+        for (i in elems.indices) {
+            swap(rn.nextInt(elems.size - 1), i)
+        }
+        this.repaint()
+    }
 
-        heightRatio = this.height / 60.0
-        widthRatio  = this.width  / elems.size.toDouble()
+    private fun drawLines(ga: Graphics2D) {
+
+        heightRatio = this.height / elems.size.toDouble()
+        widthRatio = this.width / elems.size.toDouble()
 
         for (i in elems.indices) {
-            val elemHeight = elems[i] * heightRatio
-            squares[i] = Rectangle2D.Double(
+            val currentValue = elems[i]
+            val elemHeight = currentValue * heightRatio
+            val actualSquare = Rectangle2D.Double(
                     i * widthRatio,
                     this.height - elemHeight,
                     widthRatio,
                     elemHeight
             )
-            ga.draw(squares[i])
+            ga.draw(actualSquare)
             ga.color = Color.BLACK
-            ga.fill(squares[i])
+            ga.fill(actualSquare)
         }
 
     }
@@ -73,13 +77,10 @@ class GPanel(private val elems: Array<Int>, val stepTime: Int) : JPanel() {
             return
         }
 
-        val graphic2d = graphic as Graphics2D
+        val graphic2d = graphic.create() as Graphics2D
 
-        if (inicializado) {
-            println("Dunno what goes here, and anyway this is never called...")
-        } else {
-            inicializar(graphic2d)
-        }
+        drawLines(graphic2d)
+        graphic2d.dispose()
 
     }
 

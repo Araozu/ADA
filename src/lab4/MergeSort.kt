5c5033f5fca9993c09c6f5be3fe61481883aa29c
Araozu <tbyuyfcjjll@gmail.com>
2019-11-24 19:30:50 -0500

    Cambiado algorimo

--- src/lab4/MergeSort.kt
+++ src/lab4/MergeSort.kt
@@ -2,213 +2,90 @@ package lab4
 
 class MergeSort(override val elems: Array<Int>) : SortAlgorithm {
 
-    private fun merge(mergeLeft: Int, mergeMid: Int, mergeRight: Int) {
-        val n1 = mergeMid - mergeLeft + 1
-        val n2 = mergeRight - mergeMid
-        val leftArr = Array(if (n1 >= 0) n1 else 0) { 0 }
-        val rightArr = Array(if (n2 >= 0) n2 else 0) { 0 }
+    private var doRepaint: (Int) -> Unit = {}
 
+    // Merges two subarrays of arr[].
+    // First subarray is arr[l..m]
+    // Second subarray is arr[m+1..r]
+    fun merge(arr: Array<Int>, l: Int, m: Int, r: Int) {
+        // Find sizes of two subarrays to be merged
+        val n1 = m - l + 1
+        val n2 = r - m
+
+        /* Create temp arrays */
+        val L = IntArray(n1)
+        val R = IntArray(n2)
+
+        /*Copy data to temp arrays*/
         for (i in 0 until n1) {
-            leftArr[i] = elems[mergeLeft + i]
+            L[i] = arr[l + i]
+            doRepaint(l + i)
         }
 
-        for (i in 0 until n2) {
-            rightArr[i] = elems[mergeMid + i + 1]
+        for (j in 0 until n2) {
+            R[j] = arr[m + 1 + j]
+            doRepaint(m + 1 + j)
         }
 
+        /* Merge the temp arrays */ // Initial indexes of first and second subarrays
         var i = 0
         var j = 0
-        var k = mergeLeft
-
+        // Initial index of merged subarry array
+        var k = l
         while (i < n1 && j < n2) {
-            if (leftArr[i] > rightArr[j]) {
-                elems[k] = rightArr[j]
-                j += 1
+            if (L[i] <= R[j]) {
+                arr[k] = L[i]
+                i++
             } else {
-                elems[k] = leftArr[i]
-                i += 1
-                k += 1
+                arr[k] = R[j]
+                j++
             }
+            doRepaint(k)
+            k++
         }
 
+        /* Copy remaining elements of L[] if any */
         while (i < n1) {
-            elems[k] = leftArr[i]
-            i += 1
-            k += 1
+            arr[k] = L[i]
+            doRepaint(l + i)
+            i++
+            k++
         }
 
+        /* Copy remaining elements of R[] if any */
         while (j < n2) {
-            elems[k] = rightArr[j]
-            j += 1
-            k += 1
+            arr[k] = R[j]
+            doRepaint(m + 1 + j)
+            j++
+            k++
         }
 
     }
 
+    // Main function that sorts arr[l..r] using
+    // merge()
+    fun sort(arr: Array<Int>, l: Int, r: Int) {
 
+        if (l < r) { // Find the middle point
+            val m = (l + r) / 2
 
-    fun<T> imprArr(arr: Array<T>) {
-        for (e in arr) {
-            print("$e ")
-        }
-        println()
-    }
+            // Sort first and second halves
+            sort(arr, l, m)
+            sort(arr, m + 1, r)
 
-    fun ms() {
-
-        var currentArrSize = 1
-
-        while (currentArrSize < elems.size - 1) {
-            var left = 0
-
-            while (left < elems.size - 1) {
-
-                val mid = left + currentArrSize - 1
-                val right =
-                        if (2 * currentArrSize + left - 1 > elems.size - 1) {
-                            elems.size - 1
-                        } else {
-                            2 * currentArrSize + left - 1
-                        }
-
-                merge(left, mid, right)
-                left += currentArrSize * 2
-
-            }
-
-            currentArrSize *= 2
+            // Merge the sorted halves
+            merge(arr, l, m, r)
 
         }
 
     }
 
-    private var currentArrSize = 1
-    private var left = 0
+    override fun run(doRepaint: (Int) -> Unit) {
+        this.doRepaint = doRepaint
+        sort(elems, 0, elems.size - 1)
 
-    private var isMerging = false
-    private var mergeLeft = 0
-    private var mergeMid = 0
-    private var mergeRight = 0
-
-    fun fn() {
-
-        if (isMerging) {
-
-            isMerging = false
-
-        } else {
-            if (currentArrSize < elems.size - 1) {
-
-                val mid = left + currentArrSize - 1
-                val right =
-                        if (2 * currentArrSize + left - 1 > elems.size - 1) {
-                            elems.size - 1
-                        } else {
-                            2 * currentArrSize + left - 1
-                        }
-
-                // Usar indicadores de si se esta ejecutando el merge.
-                // isMerging = true
-                merge(left, mid, right)
-
-                left += currentArrSize * 2
-
-                if (left >= elems.size - 1) {
-                    currentArrSize *= 2
-                    left = 0
-                }
-
-            }
-        }
-
-    }
-
-    fun mergeStep(swap: (Int, Int) -> Unit) {
-        val n1 = mergeMid - mergeLeft + 1
-        val n2 = mergeRight - mergeMid
-        val leftArr = Array(n1) { 0 }
-        val rightArr = Array(n2) { 0 }
-
-        for (i in 0 until n1) {
-            leftArr[i] = elems[mergeLeft + i]
-        }
-
-        for (i in 0 until n2) {
-            rightArr[i] = elems[mergeMid + i + 1]
-        }
-
-        var i = 0
-        var j = 0
-        var k = mergeLeft
-
-        while (i < n1 && j < n2) {
-            if (leftArr[i] > rightArr[j]) {
-                elems[k] = rightArr[j]
-                j += 1
-            } else {
-                elems[k] = leftArr[i]
-                i += 1
-                k += 1
-            }
-        }
-
-        while (i < n1) {
-            elems[k] = leftArr[i]
-            i += 1
-            k += 1
-        }
-
-        while (j < n2) {
-            elems[k] = rightArr[j]
-            j += 1
-            k += 1
-        }
-
-        isMerging = false
-    }
-
-    override fun step(swap: (Int, Int) -> Unit) {
-
-        if (isMerging) {
-            mergeStep(swap)
-        } else {
-            ms()
-            /*
-            if (currentArrSize < elems.size - 1) {
-
-                val mid = left + currentArrSize - 1
-                val right =
-                    if (2 * currentArrSize + left - 1 > elems.size - 1) {
-                        elems.size - 1
-                    } else {
-                        2 * currentArrSize + left - 1
-                    }
-
-                // Usar indicadores de si se esta ejecutando el merge.
-                // isMerging = true
-                merge(left, mid, right)
-
-                left += currentArrSize * 2
-
-                if (left >= elems.size - 1) {
-                    currentArrSize *= 2
-                    left = 0
-                }
-
-            }
-
-             */
-        }
-
-    }
-
-    override fun isSorted(): Boolean {
-        return if (currentArrSize >= elems.size) {
-            imprArr(elems)
-            true
-        } else {
-            false
-        }
+        // Elimina la linea roja que queda al final.
+        doRepaint(-1)
     }
 
 }
